221bce6606ece46f3c97dc2d5f462a7a535b997c
Akhand Singh <akhandsingh4906@gmail.com>
2026-04-01 22:19:15 +0530

    Exception Handlers

---
+++ src/main/java/FinanceDashboard/exception/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package FinanceDashboard.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<?> handleNotFound(ResourceNotFoundException ex) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleGeneric(Exception ex) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Error: " + ex.getMessage());
+    }
+}
